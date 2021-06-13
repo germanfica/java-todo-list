41010831c5579122a10db6782c0e02eb5a419655
German Fica <germafica@gmail.com>
2021-06-13 17:37:22 -0300

    Setup Disptacher Servlet
    I don't know why but, I think that Tomcat 10 isn't well supported with logback. The solution for me to showing up the onStartup message was to change the tomcat10x to tomcat9x in the pom.xml file.

---
+++ src/main/java/com/germanfica/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.germanfica.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.germanfica")
+public class WebConfig {
+}
