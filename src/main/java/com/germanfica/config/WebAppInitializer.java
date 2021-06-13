7f13beccc36e5c8b6c53e25f9f56d9118e1c7188
German Fica <germafica@gmail.com>
2021-06-13 17:39:40 -0300

    Removing unnecessary imports

--- src/main/java/com/germanfica/config/WebAppInitializer.java
+++ src/main/java/com/germanfica/config/WebAppInitializer.java
@@ -2,10 +2,8 @@ package com.germanfica.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
