5e26657f0cb7820a8fc0a29ceff4f6d9a3411307
German Fica <germafica@gmail.com>
2021-06-20 11:42:41 -0300

    Removing imports

--- src/main/java/com/germanfica/controller/DemoController.java
+++ src/main/java/com/germanfica/controller/DemoController.java
@@ -3,7 +3,6 @@ package com.germanfica.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DemoController {
