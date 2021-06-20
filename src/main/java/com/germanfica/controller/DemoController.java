e9d83f733c420bc505797e09191af6d1954e7539
German Fica <germafica@gmail.com>
2021-06-20 11:41:45 -0300

    View Resolver and View

--- src/main/java/com/germanfica/controller/DemoController.java
+++ src/main/java/com/germanfica/controller/DemoController.java
@@ -3,14 +3,24 @@ package com.germanfica.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DemoController {
 
+    // THIS IS THE KEY FOR API
     // http://localhost:8080/todo-list/hello
     @ResponseBody
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    // http://localhost:8080/todo-list/welcome
+    // prefix + name + suffix
+    // /WEB-INF/view/welcome.jsp
+    @GetMapping("welcome")
+    public String welcome() {
+        return "welcome";
     }
 }
