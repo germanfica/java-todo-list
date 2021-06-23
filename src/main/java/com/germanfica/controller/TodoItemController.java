868437f827c74992eefb2f11678359e0b2d9d194
German Fica <germafica@gmail.com>
2021-06-23 18:50:40 -0300

    Creating the Todo Item Controller

---
+++ src/main/java/com/germanfica/controller/TodoItemController.java
@@ -0,0 +1,25 @@
+package com.germanfica.controller;
+
+import com.germanfica.model.TodoData;
+import com.germanfica.util.Mappings;
+import com.germanfica.util.ViewNames;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class TodoItemController {
+    // == model attributes ==
+    @ModelAttribute
+    public TodoData todoData() {
+        return new TodoData();
+    }
+
+    // == handler methods ==
+    // http://localhost:8080/todo-list/items
+    @GetMapping(Mappings.ITEMS)
+    public String items() {
+        return ViewNames.ITEMS_LIST;
+    }
+
+}
---
+++ src/main/java/com/germanfica/util/Mappings.java
@@ -0,0 +1,10 @@
+package com.germanfica.util;
+
+public final class Mappings {
+    // == constants ==
+    public static final String ITEMS = "items";
+
+    // == constructors ==
+    private Mappings() {}
+
+}
